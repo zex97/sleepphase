5855a00dafc7bd2ac19813e887d125bd25ea8b12
Zeljko Lukic <e1629048@student.tuwien.ac.at>
2020-11-16 11:19:28 +0100

    Initializing project, simple bottom navigaiton view added.

---
+++ app/src/main/java/com/mse/group1/sleepphase/MainActivity.java
@@ -0,0 +1,44 @@
+package com.mse.group1.sleepphase;
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_new_alarm:
+                    mTextMessage.setText(R.string.title_new_alarm);
+                    return true;
+                case R.id.navigation_my_alarms:
+                    mTextMessage.setText(R.string.title_my_alarms);
+                    return true;
+                case R.id.navigation_settings:
+                    mTextMessage.setText(R.string.title_settings);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
