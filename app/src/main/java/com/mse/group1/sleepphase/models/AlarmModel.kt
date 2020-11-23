cbda81e6a98864ee14be3a90bfde9b9786cbfd83
Zeljko Lukic <e1629048@student.tuwien.ac.at>
2020-11-23 11:51:22 +0100

    Updated model classes for multiple alarm types.

--- app/src/main/java/com/mse/group1/sleepphase/models/AlarmModel.kt
+++ app/src/main/java/com/mse/group1/sleepphase/models/AlarmModel.kt
@@ -1,8 +1,25 @@
 package com.mse.group1.sleepphase.models
 
+import java.time.LocalDate
 import java.time.LocalTime
 
-data class AlarmModel (val type: AlarmType, val name: String, val ringAt: LocalTime, val days: Array<String>,
-                       val sound: String, val vibrate: Boolean, val snooze_enabled: Boolean,
-                       val snooze_every_min: Int, val snooze_times: Int, val turning_off_alarm: TurningOffAlarm,
-                       val checklist_bedtime: Array<ChecklistBedtimeWakeup>, val checklist_wakeup: Array<ChecklistBedtimeWakeup>)
+data class AlarmModel (var type: AlarmType,
+                       var name: String,
+                       var ringAt: LocalTime,
+                       var goal: LocalTime?,        // step by step and skip a night alarms
+                       var days: Array<String>,
+                       var skip: LocalDate?,        //only skip a night alarms
+                       var changeBy: Int?,          //only step by step alarms
+                       var everyDays: Int?,         //only step by step alarms
+                       var sound: String,
+                       var vibrate: Boolean,
+                       var snooze_enabled: Boolean,
+                       var snooze_every_min: Int,
+                       var snooze_times: Int,
+                       var turning_off_alarm: TurningOffAlarm,
+                       var checklist_bedtime: Array<ChecklistBedtimeWakeup>,
+                       var checklist_wakeup: Array<ChecklistBedtimeWakeup>) {
+    constructor() : this(AlarmType.REGULAR, "", LocalTime.MIN, null, emptyArray(), null, null, null, "",
+            false, false, 0, 0, TurningOffAlarm(TurningOffTypes.SWIPE_OVER_SCREEN, 0, 0),
+            Array(0) { ChecklistBedtimeWakeup() }, Array(0) { ChecklistBedtimeWakeup() })
+}
--- app/src/main/java/com/mse/group1/sleepphase/models/ChecklistBedtimeWakeup.kt
+++ app/src/main/java/com/mse/group1/sleepphase/models/ChecklistBedtimeWakeup.kt
@@ -1,3 +1,6 @@
 package com.mse.group1.sleepphase.models
 
-data class ChecklistBedtimeWakeup (val description: String, val checked: Boolean)
+data class ChecklistBedtimeWakeup (val description: String, val checked: Boolean) {
+    constructor() : this ("",false)
+}
+
