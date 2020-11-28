02b08b43026d4890ff22a751fe24d1dffaa1fad2
Ulrike Schaefer <ulrike.schaefer@manova.at>
2020-11-28 22:10:29 +0100

    part of the create alarm view

---
+++ app/src/main/java/com/mse/group1/sleepphase/fragments/AlarmTypeFragment.java
@@ -0,0 +1,26 @@
+package com.mse.group1.sleepphase.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.mse.group1.sleepphase.R;
+
+public class AlarmTypeFragment extends Fragment {
+
+    public AlarmTypeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my_alarms, container, false);
+    }
+
+}
