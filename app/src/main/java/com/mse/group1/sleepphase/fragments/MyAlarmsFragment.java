7171f36e4aa2dbeb142bdf52e8063bcc12d53c1c
Zeljko Lukic <e1629048@student.tuwien.ac.at>
2020-11-29 10:29:25 +0100

    Additional my alarms elements added with fake data.

--- app/src/main/java/com/mse/group1/sleepphase/fragments/MyAlarmsFragment.java
+++ app/src/main/java/com/mse/group1/sleepphase/fragments/MyAlarmsFragment.java
@@ -5,10 +5,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.TextView;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.fragment.app.Fragment;
 import com.mse.group1.sleepphase.R;
+
+import java.util.Random;
 
 
 public class MyAlarmsFragment extends Fragment {
@@ -22,18 +26,42 @@ public class MyAlarmsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View fragmentView = inflater.inflate(R.layout.fragment_my_alarms, container, false);
-        ConstraintLayout singleAlarmView = (ConstraintLayout) inflater.inflate(R.layout.alarm_layout, null);
+        ConstraintLayout singleAlarmView;
 
         LinearLayout scrollableLayout = fragmentView.findViewById(R.id.scrollableLayout);
 
         ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
         layoutParams.setMargins(0,0,0,10);
-        singleAlarmView.setLayoutParams(layoutParams);
-        scrollableLayout.addView(singleAlarmView);
+//        singleAlarmView.setLayoutParams(layoutParams);
+//        scrollableLayout.addView(singleAlarmView);
 
+        // TODO: exercise 3, read from database
         for (int i = 0; i < 6; i++) {
             singleAlarmView = (ConstraintLayout) inflater.inflate(R.layout.alarm_layout, null);
             singleAlarmView.setLayoutParams(layoutParams);
+
+            TextView alarmName = (TextView) singleAlarmView.getViewById(R.id.alarmNameTextView);
+            alarmName.setText("My Alarm " + (i+1));
+
+            TextView alarmTime = (TextView) singleAlarmView.getViewById(R.id.alarmTimeTextView);
+            alarmTime.setText("" + ((i+5)*2) + ":" + ((i+2)*7));
+
+            TextView alarmType = (TextView) singleAlarmView.getViewById(R.id.alarmTypeTextView);
+            alarmType.setText("Step By Step");
+
+            TextView alarmGoal = (TextView) singleAlarmView.getViewById(R.id.alarmGoalTextView);
+            alarmGoal.setText("Goal: " + ((i+3)*2-1) + ":" + (i*9));
+
+            TextView alarmDays = (TextView) singleAlarmView.getViewById(R.id.daysOrDateAlarmTextView);
+            alarmDays.setText("Mo-Fr");
+
+            Switch alarmSwitch = (Switch) singleAlarmView.getViewById(R.id.myAlarmsSwitch);
+            if (i%2 == 0) {
+                alarmSwitch.setChecked(true);
+            } else {
+                alarmSwitch.setChecked(false);
+            }
+
             scrollableLayout.addView(singleAlarmView);
         }
 
