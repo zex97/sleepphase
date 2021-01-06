a3a4f045cfc220aa4a5de05ef632def1fd098e39
Ulrike Schaefer <ulrike.schaefer@manova.at>
2021-01-07 00:12:23 +0100

    quiz: click listeners for buttons

--- app/src/main/java/com/mse/group1/sleepphase/quiz/QuizActivity.java
+++ app/src/main/java/com/mse/group1/sleepphase/quiz/QuizActivity.java
@@ -1,11 +1,17 @@
 package com.mse.group1.sleepphase.quiz;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.mse.group1.sleepphase.R;
+import com.mse.group1.sleepphase.addeditalarm.AddEditAlarmActivity;
+import com.mse.group1.sleepphase.alarms.AlarmsActivity;
 
 public class QuizActivity  extends AppCompatActivity {
 
@@ -28,7 +34,32 @@ public class QuizActivity  extends AppCompatActivity {
 
         viewModel = new ViewModelProvider(this).get(QuizViewModel.class);
 
-        // TODO onclicklisteners for buttons
+        Button skip = findViewById(R.id.quizSkipButton);
+        skip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                skipCklick();
+            }
+        });
+
+        Button finish = findViewById(R.id.quizFinishButton);
+        finish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finishClick();
+            }
+        });
+    }
+
+    public void skipCklick() {
+        Intent goToAlarms = new Intent(this, AlarmsActivity.class);
+        startActivity(goToAlarms);
+    }
+
+    public void finishClick() {
+        Intent goToAddAlarms = new Intent(this, AddEditAlarmActivity.class);
+        // TODO find out which alarm --> set that alarm
+        startActivity(goToAddAlarms);
     }
 
 }
