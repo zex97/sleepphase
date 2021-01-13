d44be69226cdcd589fc8e25d2836a53b5a14fabd
Zeljko Lukic <e1629048@student.tuwien.ac.at>
2021-01-13 21:27:34 +0100

    Changed turning off alarm fragment layouts.

--- app/src/main/java/com/mse/group1/sleepphase/data/Alarm.java
+++ app/src/main/java/com/mse/group1/sleepphase/data/Alarm.java
@@ -66,7 +66,7 @@ public final class Alarm {
         PendingIntent pendingIntent = PendingIntent.getBroadcast(context, getId().hashCode(), intent, 0);
         alarmManager.cancel(pendingIntent);
 
-        String string = String.format(Locale.ENGLISH,"Alarm %s cancelled for %02d:%02d", name, ringAt.getHourOfDay(), ringAt.getMinuteOfHour());
+        String string = String.format(Locale.ENGLISH,"Alarm %s canceled for %02d:%02d", name, ringAt.getHourOfDay(), ringAt.getMinuteOfHour());
         Toast.makeText(context, string, Toast.LENGTH_SHORT).show();
     }
 
